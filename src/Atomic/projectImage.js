d4d0d03e5a1b8b85f22547c1c8907a7ade075a66
acexhat <bhaskar03112000@gmail.com>
2022-02-15 21:58:53 +0530

    project dynamic

--- src/Atomic/projectImage.js
+++ src/Atomic/projectImage.js
@@ -1,13 +1,14 @@
-export const AMAZON = [
-    { img: "screenshots/ss1.png" },
-    { img: "screenshots/ss2.png" },
-    { img: "screenshots/ss3.png" },
-    { img: "screenshots/ss4.png" },
-    { img: "screenshots/ss5.png" },
-]
-
-export const GLAMGRAM = [
-    { img: "ggm1.png" },
-    { img: "ggm2.png" },
-    { img: "ggm3.png" }
-]
+export const IMAGES = {
+    GLAMGRAM: [
+        { img: "ggm1.png" },
+        { img: "ggm2.png" },
+        { img: "ggm3.png" }
+    ],
+    AMAZON: [
+        { img: "screenshots/ss1.png" },
+        { img: "screenshots/ss2.png" },
+        { img: "screenshots/ss3.png" },
+        { img: "screenshots/ss4.png" },
+        { img: "screenshots/ss5.png" },
+    ],
+}
